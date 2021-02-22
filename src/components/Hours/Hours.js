d085079c84bfff2c0f273f612f5e453522c4a25e
Yurii Sheterliak <yuriisheterliak@gmail.com>
2021-02-22 22:33:17 +0200

    fix: fix bug with displaying active tab in Hours
    Bug explanation:
    - In commit 7c16f912 to prevent Tabs component from unnecessary
      re-rendering I save them in class body (public field declaration),
      instead of creating them in render method (react creates new instances
      of component each time render method is called). It caused bug with
      these Tabs, because their props depend on state(and now these props
      are calculated only when component mounts)
    Bug fixing explanation:
    - I have changed Hours component (which creates Tabs) from class to
      function component as I need useMemo hook to memoize Tabs and change
      them only if specific state property changes

--- src/components/Hours/Hours.js
+++ src/components/Hours/Hours.js
@@ -1,4 +1,4 @@
-import React, { PureComponent } from 'react';
+import React, { useState, memo, useMemo } from 'react';
 import SimpleBar from 'simplebar-react';
 import 'simplebar/dist/simplebar.min.css';
 
@@ -14,84 +14,78 @@ import BlockHeader from '../common/BlockHeader/BlockHeader';
 import Tab from '../common/Tab/Tab';
 import Spinner from '../common/Spinner/Spinner';
 
-class Hours extends PureComponent {
-  state = {
-    timeFormat: 'h23',
-  };
+const tabsData = [
+  { name: '24-h', params: 'h23', description: '24-hour format' },
+  { name: '12-h', params: 'h12', description: '12-hour format' },
+];
 
-  changeTimeFormat = (timeFormat) => {
-    this.setState({ timeFormat: timeFormat });
-  };
+const Hours = memo((props) => {
+  const [timeFormat, setTimeFormat] = useState('h23');
 
-  tabs = [
-    <Tab
-      onClick={this.changeTimeFormat}
-      handlerParams="h23"
-      active={this.state.timeFormat === 'h23'}
-      title="24-hour format"
-      key="h23"
-    >
-      24-h
-    </Tab>,
-    <Tab
-      onClick={this.changeTimeFormat}
-      handlerParams="h12"
-      active={this.state.timeFormat === 'h12'}
-      title="12-hour format"
-      key="h12"
-    >
-      12-h
-    </Tab>,
-  ];
+  const changeTimeFormat = (timeFormat) => setTimeFormat(timeFormat);
 
-  render() {
-    let hoursData, timezone;
-    if (this.props.weather) {
-      hoursData = this.props.weather[this.props.activeDay].hourlyWeather;
-      timezone = this.props.weather[this.props.activeDay].timezone;
-    }
+  let hoursData, timezone;
+  if (props.weather) {
+    hoursData = props.weather[props.activeDay].hourlyWeather;
+    timezone = props.weather[props.activeDay].timezone;
+  }
 
-    let hours = <li className={classes.NoInfo}>No information</li>;
+  let hours = <li className={classes.NoInfo}>No information</li>;
 
-    if (hoursData && hoursData.length) {
-      hours = hoursData.map((hour, index) => {
-        const time = getFormattedTime(hour.dt, this.state.timeFormat, timezone);
-        const desc = capitalizeFirstLetters(hour.desc);
-        let temp = Math.round(hour.temp);
-        if (this.props.units === 'fahrenheit') {
-          temp = Math.round(toFahrenheit(hour.temp));
-        }
+  if (hoursData && hoursData.length) {
+    hours = hoursData.map((hour, index) => {
+      const time = getFormattedTime(hour.dt, timeFormat, timezone);
+      const desc = capitalizeFirstLetters(hour.desc);
+      let temp = Math.round(hour.temp);
+      if (props.units === 'fahrenheit') {
+        temp = Math.round(toFahrenheit(hour.temp));
+      }
 
-        return (
-          <Hour
-            key={index}
-            time={time}
-            temp={temp}
-            weatherID={hour.id}
-            desc={desc}
-            active={isCurrentHour(hour.dt)}
-          />
-        );
-      });
-    }
-
-    if (this.props.isLoading) {
-      hours = (
-        <div className={classes.Error}>
-          <Spinner big />
-        </div>
+      return (
+        <Hour
+          key={index}
+          time={time}
+          temp={temp}
+          weatherID={hour.id}
+          desc={desc}
+          active={isCurrentHour(hour.dt)}
+        />
       );
-    }
+    });
+  }
 
-    return (
-      <div className={classes.Hours}>
-        <BlockHeader>{this.tabs}</BlockHeader>
-        <SimpleBar className={classes.List} timeout="200">
-          <ul>{hours}</ul>
-        </SimpleBar>
+  if (props.isLoading) {
+    hours = (
+      <div className={classes.Error}>
+        <Spinner big />
       </div>
     );
   }
-}
+
+  const tabs = useMemo(
+    () =>
+      tabsData.map((tabData, index) => (
+        <Tab
+          onClick={changeTimeFormat}
+          handlerParams={tabData.params}
+          active={timeFormat === tabData.params}
+          title={tabData.description}
+          key={index}
+        >
+          {tabData.name}
+        </Tab>
+      )),
+    [timeFormat]
+  );
+
+  return (
+    <div className={classes.Hours}>
+      <BlockHeader>{tabs}</BlockHeader>
+      <SimpleBar className={classes.List} timeout="200">
+        <ul>{hours}</ul>
+      </SimpleBar>
+    </div>
+  );
+});
 
 export default Hours;
