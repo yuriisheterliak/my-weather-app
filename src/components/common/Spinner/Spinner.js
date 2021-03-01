da3f512090ce35af54211ea191efa3c9dbe3e701
Yurii Sheterliak <yuriisheterliak@gmail.com>
2021-03-01 14:41:28 +0200

    chore: update all the Node dependencies to their latest version

--- src/components/common/Spinner/Spinner.js
+++ src/components/common/Spinner/Spinner.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
-import { ReactComponent as Spinner } from '../../../assets/images/spinner.svg';
+import { ReactComponent as SpinnerSvg } from '../../../assets/images/spinner.svg';
 import classes from './Spinner.module.scss';
 
-export default (props) => {
+const Spinner = (props) => {
   let spinnerClasses = [classes.Spinner];
   if (props.small) spinnerClasses.push(classes.Small);
   if (props.big) spinnerClasses.push(classes.Big);
 
-  return <Spinner className={spinnerClasses.join(' ')} />;
+  return <SpinnerSvg className={spinnerClasses.join(' ')} />;
 };
+
+export default Spinner;
